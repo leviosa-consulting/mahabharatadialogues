aa2bb61145867de54fef066b897e87ad706127b8
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-12-12 17:30:49 +0530

    change

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -1,16 +1,18 @@
-import * as admin from "firebase-admin";
+import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const raw = process.env.FIREBASE_ADMIN_CREDENTIALS!;
+  const serviceAccount = JSON.parse(raw);
+
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET, 
   });
 }
 
 export const adminDB = admin.firestore();
+
+// IMPORTANT FIX — use bucket()
 export const adminStorage = admin.storage().bucket();
-export default admin;
