4b26e078b46b523351f73c6aa7f935fcb32a5bd7
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2026-02-10 11:08:14 +0530

    about us page changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,47 @@ import Navbar from '@/components/Navbar'
 import NavbarScroll from '@/components/NavbarScroll'
 import { merri } from '../fonts/merri'
 import CustomButton from '@/components/CustomButton'
+import Footer from '@/components/Footer'
 
 const AboutPage = () => {
+  const team = [
+    {
+      name: 'Abhilash Purohit',
+      role: 'STORIES | AUTHOR | SPEAKER',
+    },
+    {
+      name: 'Rakshith Bhagvwath',
+      role: 'STORIES | AUTHOR | SPEAKER',
+    },
+    {
+      name: 'Radha Sawana',
+      role: 'STORIES | AUTHOR | SPEAKER',
+    },
+    {
+      name: 'Namitha Vijaykumar',
+      role: 'STORIES | AUTHOR | SPEAKER',
+    },
+    {
+      name: 'Saishraddha Balla',
+      role: 'STORIES | AUTHOR | SPEAKER',
+    },
+    {
+      name: 'Deepika',
+      role: 'STORIES | AUTHOR | SPEAKER',
+    },
+    {
+      name: 'Nikhil Joshi',
+      role: 'STORIES | AUTHOR | SPEAKER',
+    },
+    {
+      name: 'Saurabh Dubey',
+      role: 'STORIES | AUTHOR | SPEAKER',
+    },
+    {
+      name: 'Ruchika Kadam',
+      role: 'STORIES | AUTHOR | SPEAKER',
+    },
+  ]
   return (
     <div>
       <div>
@@ -72,76 +111,82 @@ const AboutPage = () => {
       </div>
 
       {/* third section */}
-      <div className="mx-4 xl:mx-30 my-30">
-        <div className='flex justify-center items-center my-6 '>
-            <h2 className={`${merri.className} font-bold text-[#1D5C75] text-[18px]`}>CORE TEAM</h2>
+      <div className=" my-30">
+        <div className="flex flex-col md:flex-row justify-center items-center my-6 ">
+          <h2
+            className={`${merri.className} font-bold text-[#1D5C75] text-[18px]`}
+          >
+            CORE TEAM
+          </h2>
         </div>
-        <div className="w-full flex justify-between gap-6">
-          <div className="relative w-[400px] h-[440px] flex flex-col items-center justify-center">
-            <div className="absolute top-0 w-full h-[71%] bg-[#D9D9D9]/60"></div>
-            <img
-              src="/teamImg.png"
-              alt="teamImg"
-              className="w-full h-full object-contain "
-            />
-            <div className="w-full flex flex-col justify-center items-center h-full -mt-32 p-4 bg-gradient-to-b from-[#1D5C75] to-[#1D5C75]/50">
-              <h2
-                className={`${merri.className} italic font-extrabold text-[34px] text-white`}
+        <div className="w-full">
+          {/* GRID CONTAINER */}
+          <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center max-w-7xl">
+            {team.map((item, index) => (
+              <div
+                key={index}
+                className="relative w-full max-w-[400px] h-[440px] flex flex-col items-center justify-center"
               >
-                Abhilash Purohit
-              </h2>
+                {/* Image background */}
+                <div className="absolute top-0 w-full h-[71%] bg-[#D9D9D9]/60" />
 
-              <h3
-                className={`${merri.className} font-normal text-[24px] text-white`}
-              >
-                STORIES | AUTHOR | SPEAKER
-              </h3>
-            </div>
-          </div>
+                <img
+                  src="/teamImg.png"
+                  alt={item.name}
+                  className="w-full h-full object-contain"
+                />
 
-          <div className="relative w-[400px] h-[440px] flex flex-col items-center justify-center">
-            <div className="absolute top-0 w-full h-[71%] bg-[#D9D9D9]/60"></div>
-            <img
-              src="/teamImg.png"
-              alt="teamImg"
-              className="w-full h-full object-contain "
-            />
-            <div className="w-full flex flex-col justify-center items-center h-full -mt-32 p-4 bg-gradient-to-b from-[#1D5C75] to-[#1D5C75]/50">
-              <h2
-                className={`${merri.className} italic font-extrabold text-[34px] text-white`}
-              >
-                Rakshith Bhagvwath
-              </h2>
+                {/* Bottom gradient content */}
+                <div className="w-full flex flex-col justify-center items-center h-full -mt-32 p-4 bg-gradient-to-b from-[#1D5C75] to-[#1D5C75]/50 text-center">
+                  <h2
+                    className={`${merri.className} italic font-extrabold text-[28px] md:text-[34px] text-white`}
+                  >
+                    {item.name}
+                  </h2>
 
-              <h3
-                className={`${merri.className} font-normal text-[24px] text-white`}
-              >
-                STORIES | AUTHOR | SPEAKER
-              </h3>
-            </div>
-          </div>
-          <div className="relative w-[400px] h-[440px] flex flex-col items-center justify-center">
-            <div className="absolute top-0 w-full h-[71%] bg-[#D9D9D9]/60"></div>
-            <img
-              src="/teamImg.png"
-              alt="teamImg"
-              className="w-full h-full object-contain "
-            />
-            <div className="w-full flex flex-col justify-center items-center h-full -mt-32 p-4 bg-gradient-to-b from-[#1D5C75] to-[#1D5C75]/50">
-              <h2
-                className={`${merri.className} italic font-extrabold text-[34px] text-white`}
-              >
-                Radha Sawana
-              </h2>
-
-              <h3
-                className={`${merri.className} font-normal text-[24px] text-white`}
-              >
-                STORIES | AUTHOR | SPEAKER
-              </h3>
-            </div>
+                  <h3
+                    className={`${merri.className} font-normal text-[18px] md:text-[24px] text-white`}
+                  >
+                    {item.role}
+                  </h3>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
+      </div>
+
+      {/* fourth section */}
+      <div className="my-30">
+        <div className="flex justify-center items-center my-6 ">
+          <h2
+            className={`${merri.className} font-bold text-[#1D5C75] text-[18px]`}
+          >
+            COLLABORATORS
+          </h2>
+        </div>
+        <div className="w-full mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-4">
+          <div className="bg-gray-400 w-full h-80"></div>
+          <div className="bg-gray-200 w-full h-80"></div>
+          <div className="bg-gray-200 w-full h-80"></div>
+          <div className="bg-gray-200 w-full h-80"></div>
+
+          <div className="bg-gray-400 w-full h-80"></div>
+          <div className="bg-gray-200 w-full h-80"></div>
+          <div className="bg-gray-200 w-full h-80"></div>
+          <div className="bg-gray-200 w-full h-80"></div>
+        </div>
+      </div>
+
+      <div className=''style={{
+          backgroundImage: `
+    linear-gradient(#1D5C75, #1D5C7580),
+    url('/MD-Texture_BG_Blue-01-04.png')
+  `,
+          backgroundRepeat: 'repeat',
+          backgroundSize: '240px 240px',
+        }}>
+        <Footer />
       </div>
     </div>
   )
